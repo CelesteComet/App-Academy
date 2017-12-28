db6aa2f310adc9a715a4618100f46eaf02b4bebb
Bruce Wong <brucewong21@gmail.com>
2017-12-28 08:45:44 -0800

    finish W4D1

---
+++ W4D1/app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def index
+    # your code here
+    @books = Book.all 
+  end
+
+  def new
+    # your code here
+  end
+
+  def create
+    # your code here
+    Book.create({title: book_params['title'], author: book_params['author']})
+    redirect_to '/books'
+  end
+
+  def destroy
+    # your code here
+    Book.find(params[:id]).destroy
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+end
