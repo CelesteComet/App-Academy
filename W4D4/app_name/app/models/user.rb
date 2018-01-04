031516f95313f5595716880fd11a5c3fcb0e9199
Bruce Wong <brucewong21@gmail.com>
2018-01-04 01:09:41 -0800

    finishhw

---
+++ W4D4/app_name/app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  validates :username, :session_token, :password_digest, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    User.find('WHERE username = ? AND password = ?', username, password)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+end
